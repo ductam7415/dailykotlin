8e1ddadbf36603866361e32a146a96e303115468
dailyhomework29-4 <tamnguyenduc@gmail.com>
2021-08-13 22:09:25 +0700

    bai tap 12/8

---
+++ src/number4.kt
@@ -0,0 +1,9 @@
+fun main(){
+    println("Anh em nhớ điền số là số hộ e với ạ")
+    var number = readLine()!!.toInt()
+    if(number%2==0){
+        println("kết quả là số chẵn của anh hết 10$")
+    }else{
+        println("kết quả là số lẻ của anh hết 11$")
+    }
+}
